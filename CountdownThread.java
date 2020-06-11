5e636843b1f7ec3c8236b925c87e0b08bf6d3415
Dawid Ciesielski <dawidciesielski88@gmail.com>
2020-06-11 09:46:46 +0200

    Add files via upload

---
+++ CountdownThread.java
@@ -0,0 +1,13 @@
+package com.dawidciesielski;
+
+public class CountdownThread extends Thread {
+    private Countdown threadCountdown;
+
+    public CountdownThread(Countdown countdown) {
+        threadCountdown = countdown;
+    }
+
+    public void run() {
+        threadCountdown.doCountdown();
+    }
+}
